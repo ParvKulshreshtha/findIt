2aff2332099980d0f25e7c1b01d72e0cf5d0e7b9
Parv <kulparv@gmail.com>
2024-01-12 14:48:07 +0530

    setup

--- App.js
+++ App.js
@@ -1,9 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback } from 'react';
+
+SplashScreen.preventAutoHideAsync();
+
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Karla': require('./assets/Karla/static/Karla-Regular.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Text>Open up App.js to start working on your app!</Text>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -12,6 +33,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    fontFamily:'Karla',
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
